8cf1fe598e3c0c0544746e8423690760bc7e408f
Ed Bennett <e.j.bennett@swansea.ac.uk>
2022-06-01 00:22:24 +0100

    tidy up tables 2,3,8-12; towards #12

--- src/tabulate_scale.sh
+++ src/tabulate_scale.sh
@@ -6,7 +6,31 @@ SCALES_FILE=$1
 T0_FILE=$2
 W0_FILE=$3
 
-awk '{print "$",$3,"$&$",$4,"$&$",$5,"$&$",$6,"$ \\\\"}' ${SCALES_FILE} > ${T0_FILE}
-sed -i -e '$s/\\\\//' ${T0_FILE}
-awk '{print "$",$3,"$&$",$7,"$&$",$8,"$&$",$9,"$ \\\\"}' ${SCALES_FILE} > ${W0_FILE}
-sed -i -e '$s/\\\\//' ${W0_FILE}
+cat > ${T0_FILE} <<'EOF'
+\begin{tabular}{|c|c|c|c|}
+\hline
+\hline
+$\beta$ & $\mathcal{E}_0$ & $\sqrt{t_0}/a$ (pl.) & $\sqrt{t_0}/a$ (cl.) \\
+\hline
+EOF
+
+cat > ${W0_FILE} <<'EOF'
+\begin{tabular}{|c|c|c|c|}
+\hline
+\hline
+$\beta$ & $\mathcal{W}_0$ & $w_0/a$ (pl.) & $w_0/a$ (cl.) \\
+\hline
+EOF
+
+awk '{print "$",$3,"$&$",$4,"$&$",$5,"$&$",$6,"$ \\\\"}' ${SCALES_FILE} >> ${T0_FILE}
+awk '{print "$",$3,"$&$",$7,"$&$",$8,"$&$",$9,"$ \\\\"}' ${SCALES_FILE} >> ${W0_FILE}
+
+cat >> ${T0_FILE} <<EOF
+\hline
+\end{tabular}
+EOF
+
+cat >> ${W0_FILE} <<EOF
+\hline
+\end{tabular}
+EOF
