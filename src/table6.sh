e3f597966cdba0e9dddf406baf9d4002ed6f2a86
Ed Bennett <e.j.bennett@swansea.ac.uk>
2022-06-06 14:36:34 +0100

    fix latex issue with column headings in table 6

--- src/table6.sh
+++ src/table6.sh
@@ -6,8 +6,8 @@ cat <<'EOF'
 \begin{tabular}{|c|cc|cc|}
 \hline
 \hline
-$N_c$ & $\chi_L t_0^2(a=0) & \tilde{\mathcal{X}}^2 & $\chi_L t_0^2(a=0) & \tilde{\mathcal{X}}^2 \\
- & $c_e = 0.225$ & & c_e = 0.5 & \\
+$N_c$ & $\chi_L t_0^2(a=0)$ & $\tilde{\mathcal{X}}^2$ & $\chi_L t_0^2(a=0)$ & $\tilde{\mathcal{X}}^2$ \\
+ & $c_e = 0.225$ & & $c_e = 0.5$ & \\
 \hline
 EOF
 
@@ -16,8 +16,8 @@ paste -d' ' <(printf "\$2\$\n\$4\$\n\$6\$\n\$8\$\n") $1 $2 <(printf '\\\\\n\\\\\
 cat <<'EOF'
 \hline
 \hline
-$N_c$ & $\chi_L w_0^4(a=0) & \tilde{\mathcal{X}}^2 & $\chi_L w_0^4(a=0) & \tilde{\mathcal{X}}^2 \\
- & $c_w = 0.225$ & & c_w = 0.5 & \\
+$N_c$ & $\chi_L w_0^4(a=0)$ & $\tilde{\mathcal{X}}^2$ & $\chi_L w_0^4(a=0)$ & $\tilde{\mathcal{X}}^2$ \\
+ & $c_w = 0.225$ & & $c_w = 0.5$ & \\
 \hline
 EOF
 
